5472205a2449b24c28cbd6d7f81e9ddb309206dc
Carl Waldbieser <cwaldbieser@gmail.com>
2020-01-02 17:02:36 -0500

    Code cleanup; fido2 stuff-- not quite working, yet.

--- awsprocesscreds/duo_mfa.py
+++ awsprocesscreds/duo_mfa.py
@@ -1,7 +1,9 @@
 
+import base64
 import getpass
 import json
 import logging
+import struct
 import time
 from urllib.parse import (
     quote_plus,
@@ -10,6 +12,7 @@ from urllib.parse import (
 )
 import xml.etree.cElementTree as ET
 from .html_parsers import FormParser, FrameParser
+from .fido2 import present_challenge_to_authenticator
 
 logger = logging.getLogger(__name__)
 
@@ -48,21 +51,29 @@ def _perform_duo_mfa_flow(parent, login_url, response, duo_device, duo_factor):
     frame = parser.get_frame_by_id('duo_iframe')
     host = frame['data-host']
     logger.debug("DUO host: {}".format(host))
+    duo_poll_seconds = parent.DUO_POLL_SECONDS
+    status_endpoint = urljoin("https://{}".format(host), "/frame/status")
+    logger.debug("DUO status endpoint: {}".format(status_endpoint))
     duo_auth_version = parent.DUO_AUTH_VERSION
     duo_sig, app = tuple(frame['data-sig-request'].split(':'))
-    frame_url = "https://{}/frame/web/v1/auth?tx={}&parent={}&v={}".format(host, duo_sig, quote_plus(login_url), duo_auth_version)
-    logger.debug("DUO frame auth URL: {}".format(frame_url))
-    response = parent._requests_session.get(frame_url, verify=True)
-    duo_form_html_node = parent._parse_form_from_html(response.text, form_index=parent.DUO_FORM_INDEX)
+    auth_endpoint = "https://{}/frame/web/v1/auth?tx={}&parent={}&v={}".format(host, duo_sig, quote_plus(login_url), duo_auth_version)
+    logger.debug("DUO auth_endpoint: {}".format(auth_endpoint))
+    logger.debug("DUO HTTP GET auth_endpoint ...")
+    raw_response = parent._requests_session.get(auth_endpoint, verify=True)
+    logger.debug("DUO parsing auth_endpoint response ...")
+    duo_form_html_node = parent._parse_form_from_html(raw_response.text, form_index=parent.DUO_FORM_INDEX)
     payload = dict((tag.attrib['name'], tag.attrib.get('value', ''))
                    for tag in duo_form_html_node.findall(".//input"))
-    response = parent._send_form_post(frame_url, payload)
-    duo_form_html_node = parent._parse_form_from_html(response.text, form_index=parent.DUO_FORM_INDEX)
+    logger.debug("DUO HTTP POST to auth_endpoint.  payload: {}".format(payload))
+    raw_response = parent._send_form_post(auth_endpoint, payload)
+    logger.debug("DUO Got response from auth_endpoint.")
+    duo_form_html_node = parent._parse_form_from_html(raw_response.text, form_index=parent.DUO_FORM_INDEX)
     payload = dict((tag.attrib['name'], tag.attrib.get('value', ''))
                    for tag in duo_form_html_node.findall(".//input"))
-    action = duo_form_html_node.attrib.get('action', '')
-    frame_url = urljoin("https://{}".format(host), action)
-    logger.debug("DUO prompt endpoint: {}".format(frame_url))
+    sid = payload['sid']
+    logger.debug("DUO sid: {}".format(sid))
+
+    # Get prompt endpoint; get txid
     payload['device'] = duo_device
     if duo_factor == "webauthn":
         payload['factor'] = "WebAuthn Credential"
@@ -70,45 +81,89 @@ def _perform_duo_mfa_flow(parent, login_url, response, duo_device, duo_factor):
         payload['factor'] = duo_factor
     if duo_factor == 'Passcode':
         payload['passcode'] = getpass.getpass("Passcode: ")
+    action = duo_form_html_node.attrib.get('action', '')
+    prompt_endpoint = urljoin("https://{}".format(host), action)
+    logger.debug("DUO prompt endpoint: {}".format(prompt_endpoint))
     logger.debug("DUO prompt endpoint payload: {}".format(payload))
-    response = parent._send_form_post(frame_url, payload)
+    response = parent._send_form_post(prompt_endpoint, payload)
     logger.debug("Duo prompt endpoint response: {}".format(response.text))
     response = json.loads(response.text)
     if response.get('stat') != 'OK':
-        raise Exception("POST to Duo prompt resulted in error: {}".format(response))
-    txid = response.get('response', {}).get('txid')
-    sid = payload['sid']
-    payload = dict(sid=sid, txid=txid)
-    duo_status_url = urljoin("https://{}".format(host), "/frame/status")
-    duo_poll_seconds = parent.DUO_POLL_SECONDS
-    logger.debug("DUO status endpoint: {}".format(duo_status_url))
+        raise Exception("DUO POST to prompt endpoint resulted in error: {}".format(response))
+    txid = response.get("response", {}).get("txid")
+    logger.debug("DUO txid: {}".format(txid))
+
     if duo_factor == 'webauthn':
-        logger.debug("Getting challenge from Duo prompt endpoint ...")
+        logger.debug("DUO Getting challenge from status endpoint ...")
         logger.debug("DUO device: {}".format(duo_device))
-        #payload['device'] = duo_device
+        payload['device'] = duo_device
         payload['factor'] = "WebAuthn Credential"
-        payload['device'] = "WA5NXAL405S765X5WJFA"
-        logger.debug("DUO status URL: {}".format(duo_status_url))
+        payload['txid'] = txid
+        logger.debug("DUO status endpoint: {}".format(status_endpoint))
         logger.debug("DUO payload: {}".format(payload))
-        raw_response = parent._send_form_post(duo_status_url, payload)
+        raw_response = parent._send_form_post(status_endpoint, payload)
+        logger.debug("DUO got response from status endpoint.")
         try:
             response = json.loads(raw_response.text)
         except Exception as ex:
             logger.error("DUO Error decoding JSON response from prompt endpoint: {}".format(ex))
             raise
-        # Example response
-        # {"stat": "OK", "response": {"webauthn_credential_request_options": {"allowCredentials": [{"transports": ["usb", "nfc", "ble"], "id": "_HbP_XU2bqAFEi6spjC6RT00JysSJZPYg-U32zK_7i4mnzrd-SPQRhJh12Olk0xHXQL720PTvLEBfvhhla3-8A", "type": "public-key"}], "rpId": "duosecurity.com", "timeout": 60000, "challenge": "YagLoMMgBn8UlOsm3do5yVE55erM1XxJ", "sessionId": "BY_Y_j5zNHM4aPlu25JCqGXJqI8YgYuaYYloB2hR2jc"}, "status": "Use your Security Key to log in...", "status_code": "webauthn_sent"}} 
         if response.get('stat') != 'OK':
+            logger.error("DUO POST for credential challenge resulted in error: {}".format(response))
             raise Exception("DUO POST for credential challenge resulted in error: {}".format(response))
         webauthn_opts = response.get('response', {}).get('webauthn_credential_request_options') 
-        # TODO: submit options to authenticator, pass results to Duo endpoint. 
-        raise Exception("webauthn_opts: {}".format(webauthn_opts))
+        origin = 'https://api-6bfb7da1.duosecurity.com'
+        logger.info("Getting assertion from authenticator ...")
+        assertion = present_challenge_to_authenticator(webauthn_opts, origin)
+        logger.debug("DUO authenticator assertion: {}".format(assertion))
+        payload['device'] = "webauthn_credential"
+        payload['factor'] = "webauthn_finish"
+        auth_data = assertion.auth_data
+        b64_cred_id = base64.urlsafe_b64encode(assertion.credential['id']).decode('ascii')
+        response_data = json.dumps(dict(
+            sessionId=webauthn_opts['sessionId'],
+            id=b64_cred_id,
+            rawId=b64_cred_id,
+            type=assertion.credential['type'],
+            authenticatorData=base64.b64encode(auth_data.rp_id_hash + struct.pack(">BI", auth_data.flags, auth_data.counter)).decode('ascii'),
+            clientDataJSON=base64.b64encode(json.dumps(dict(
+                challenge=webauthn_opts['challenge'],
+                clientExtensions={},
+                hashAlgorithm="SHA-256",
+                origin=origin,
+                type="webauthn.get"
+            )).encode('ascii')).decode('ascii'),
+            signature=assertion.signature.hex(),
+        ))
+        logger.debug("DUO webauthn response_data: {}".format(response_data))
+        payload['response_data'] = response_data
+        logger.debug("DUO prompt URL: {}".format(prompt_endpoint))
+        logger.debug("DUO payload : {}".format(payload))
+        raw_response = parent._send_form_post(prompt_endpoint, payload)
+        logger.debug("DUO response received from webauthn_finish endpoint: {}".format(raw_response.text))
+        try:
+            response = json.loads(raw_response.text)
+        except Exception as ex:
+            logger.error("DUO Could not decode webauthn response.")
+            raise ex
+        stat = response.get('stat', '')
+        if stat!= 'OK':
+            logger.error("DUO webauthn stat: {}".format(stat))
+            raise Exception("DUO webauthn stat: {}".format(stat))
+        txid = response.get('response', {}).get('txid')
+
+    payload = dict(sid=sid, txid=txid)
+    logger.debug("DUO poll yield time: {}".format(duo_poll_seconds))
     while True:
-        raw_response = parent._send_form_post(duo_status_url, payload)
+        logger.debug("DUO polling for status ...")
+        logger.debug("DUO status_endpoint: {}".format(status_endpoint))
+        logger.debug("DUO status payload: {}".format(payload))
+        raw_response = parent._send_form_post(status_endpoint, payload)
+        logger.debug("DUO Got response from status endpoint.")
         response = json.loads(raw_response.text)
         if response.get('stat') != 'OK':
             logger.error("DUO stat code: {}".format(response.get('stat')))
-            raise Exception("POST to Duo status URL resulted in error: {}".format(raw_response.text))
+            raise Exception("POST to Duo status endpoint resulted in error: {}".format(raw_response.text))
         status_code = response.get('response', {}).get('status_code') 
         if status_code == 'pushed':
             logger.debug("DUO status code == 'pushed'")
@@ -123,9 +178,9 @@ def _perform_duo_mfa_flow(parent, login_url, response, duo_device, duo_factor):
             logger.error("DUO raw response: {}".format(raw_response.text))
             raise Exception("Duo returned status code: `{}`".format(status_code))
     payload = dict(sid=sid)
-    duo_result_url = urljoin("https://{}".format(host), result_url)
-    logger.debug("DUO status result endpoint: {}".format(duo_result_url))
-    raw_response = parent._send_form_post(duo_result_url, payload)
+    status_result_endpoint = urljoin("https://{}".format(host), result_url)
+    logger.debug("DUO status result endpoint: {}".format(status_result_endpoint))
+    raw_response = parent._send_form_post(status_result_endpoint, payload)
     logger.debug("DUO status result endpoint response: {}".format(raw_response.text))
     response = json.loads(raw_response.text)
     cookie = response['response']['cookie']
